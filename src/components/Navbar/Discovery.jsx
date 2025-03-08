aa396b63f3fdb36039584d8966cf3a9236280731
Enes Sirin <sefsirin55@gmail.com>
2025-03-08 23:38:31 +0300

    Discovery Menu List

---
+++ src/components/Navbar/Discovery.jsx
@@ -0,0 +1,79 @@
+import React, { useState, useEffect } from "react";
+import MenuData from "../../api/menuData.json";
+function Discovery({ menuHover, setMenuHover }) {
+  const [menuList, setMenuList] = useState([]);
+  const [menuIndex, setMenuIndex] = useState(null);
+  const [subMenuHover, setSubMenuHover] = useState(false)
+  useEffect(() => {
+    setMenuList(MenuData.navbar);
+  }, []);
+
+  useEffect(() => {
+    console.log(menuList[menuIndex]?.submenu);
+  }, [menuIndex]);
+  return (
+    <div
+      className={`discovery-all-list ${menuHover ? "" : "d-none"}`}
+      onMouseLeave={() => setMenuHover(false)}
+    >
+      <div className="discovery-list">
+        <ul className="discovery-list-content">
+          <p className="discovery-header">Sertifikalara göz atın</p>
+          {menuList.map((item, index) => (
+            <div
+              key={index}
+              data-id={index}
+              onMouseEnter={() => setMenuIndex(index)}
+            >
+              <a href={item.href}>
+                <li className={`discovery-list-item ${index === menuIndex ? "discovery-item-active" : ""}`}>
+                  {item.title}
+                  <i className="fa-solid fa-chevron-right"></i>
+                </li>
+              </a>
+
+              {index === 0 ? <hr style={{ marginTop: "6px" }} /> : null}
+            </div>
+          ))}
+        </ul>
+      </div>
+      <div
+        className={`discovery-list discovery-list-second ${
+          menuIndex === null ? "d-none" : "d-flex"
+        }`}
+      >
+        <ul className="discovery-list-content">
+          {menuIndex !== null &&
+            menuList[menuIndex]?.submenu.map((item, index) => (
+              <div key={index} data-id={index} onMouseEnter={() => setSubMenuHover(true)}>
+                <a href={item.href}>
+                  <li className="discovery-list-item">
+                    {item.title}
+                    <i className="fa-solid fa-chevron-right"></i>
+                  </li>
+                </a>
+              </div>
+            ))}
+        </ul>
+      </div>
+      <div className={`discovery-list discovery-list-second ${subMenuHover ? "" : "d-none"}`} onMouseLeave={() => setSubMenuHover(false)}>
+        <ul className="discovery-list-content">
+          <li className="discovery-list-item">
+            <a href="#cer">Lorem Ipsum</a>
+            <i className="fa-solid fa-chevron-right"></i>
+          </li>
+          <li className="discovery-list-item">
+            <a href="#cer">Lorem Ipsum</a>
+            <i className="fa-solid fa-chevron-right"></i>
+          </li>
+          <li className="discovery-list-item">
+            <a href="#cer">Lorem Ipsum</a>
+            <i className="fa-solid fa-chevron-right"></i>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Discovery;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React,{useState} from "react";
 import "./navbar.css";
+import Discovery from "./Discovery";
 function Navbar() {
+  const [menuHover, setMenuHover] = useState(false);
   return (
-    <nav className="">
+    <nav className="container-fluid position-relative">
       <div className="logo">
         <img
           className="logo"
@@ -10,26 +12,43 @@ function Navbar() {
           alt="Udemy"
         />
       </div>
-      <button className="discovery-btn nav-btn">Keşfedin</button>
+      <button className="discovery-btn nav-btn" onMouseEnter={() => setMenuHover(true)}>Keşfedin</button>
       <div className="search-input">
-      <i class="fa-solid fa-magnifying-glass"></i>
-      <input type="text" name="" id="header-search" placeholder="Dilediğiniz şeyi arayın" />
+        <i className="fa-solid fa-magnifying-glass"></i>
+        <input
+          type="text"
+          name=""
+          id="header-search"
+          placeholder="Dilediğiniz şeyi arayın"
+        />
       </div>
 
       <div className="menu-right-side">
-        <a className="nav-btn d-none d-xl-flex menu-link" href="#udemy-business">Udemy Business</a>
-        <a className="nav-btn d-none d-lg-flex" href="#udemy-business">Udemy'de Eğitim Verin</a>
+        <a
+          className="nav-btn d-none d-xl-flex menu-link"
+          href="#udemy-business"
+        >
+          Udemy Business
+        </a>
+        <a className="nav-btn d-none d-lg-flex" href="#udemy-business">
+          Udemy'de Eğitim Verin
+        </a>
 
         <button className="nav-btn cart-btn">
-          <i class="fa-solid fa-cart-shopping"></i>
+          <i className="fa-solid fa-cart-shopping"></i>
         </button>
-        <button className="nav-btn" id="sign-in-btn">Oturum aç</button>
-        <button className="nav-btn" id="sign-up-btn">Kaydol</button>
+        <button className="nav-btn" id="sign-in-btn">
+          Oturum aç
+        </button>
+        <button className="nav-btn" id="sign-up-btn">
+          Kaydol
+        </button>
 
         <button className="nav-btn lang-btn">
-          <i class="fa-solid fa-globe"></i>
+          <i className="fa-solid fa-globe"></i>
         </button>
       </div>
+      <Discovery menuHover={menuHover} setMenuHover={setMenuHover} />
     </nav>
   );
 }
